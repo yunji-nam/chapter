712e86b6df6b47b9ad1dca8d53c7200b3e92704f
yunji <dbswl4342@gmail.com>
2025-02-04 22:11:02 +0900

    fix

--- src/test/java/com/example/chapter/ChapterApplicationTests.java
+++ src/test/java/com/example/chapter/ChapterApplicationTests.java
@@ -1,9 +1,7 @@
 package com.example.chapter;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ChapterApplicationTests {
 
     @Test
